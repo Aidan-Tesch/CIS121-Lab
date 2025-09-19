52dbf893a5c19ba13bb2d56a8f2708054b2af0f9
Aidan Tesch <bt4538lm@go.minnstate.edu>
2025-09-19 10:48:34 -0500

    Week 4
    Question 3 done

--- Week 4/Week 4.py
+++ Week 4/Week 4.py
@@ -1,14 +1,21 @@
 # Question 1
-biggest = int(input("Enter the largest number: "))
-smallest = int(input("Enter the smallest number: "))
-count = 0
-while biggest/2 > smallest:
-    biggest /= 2
-    count+=1
-print(f'The Biggest number was divided {count} times.')
+#biggest = int(input("Enter the largest number: "))
+#smallest = int(input("Enter the smallest number: "))
+#count = 0
+#while biggest/2 > smallest:
+    #biggest /= 2
+    #count+=1
+#print(f'The Biggest number was divided {count} times.')
 
 # Question 2
-user_word = input("Enter any word: ")
-for i in range(len(user_word)):
-    if not(i % 2 == 0):
-        print(user_word[i], end="")
+#user_word = input("Enter any word: ")
+#for i in range(len(user_word)):
+    #if not(i % 2 == 0):
+        #print(user_word[i], end="")
+
+# Question 3
+for i in range(37, 1051):
+    if i % 2 == 0:
+        print(i)
+
+
